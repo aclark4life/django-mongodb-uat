5ee220fdd842a604090e0ff4ee98c004305bc6c9
Jeffrey A. Clark <aclark@aclark.net>
2025-12-08 14:33:07 -0500

    Configure database via MONGODB_URI

--- uat/uat/settings.py
+++ uat/uat/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,8 +75,9 @@ WSGI_APPLICATION = 'uat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_mongodb_backend',
+        'NAME': 'uat',
+        'HOST': os.getenv('MONGODB_URI', 'localhost'),
     }
 }
 
